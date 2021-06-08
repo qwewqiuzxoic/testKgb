b95182f94da693e70ea2dfa464bc16a93fe027c2
yugyeongO <61178573+yugyeongO@users.noreply.github.com>
2021-06-08 09:40:06 +0900

    Fix Manage12.js
    품질평가_브랜드평가_리스트 페이지 추가

--- src/pages/Manage12.js
+++ src/pages/Manage12.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Head from '../components/commonStyle/Head';
 import ImgBoardWrap from '../components/Manage12/ImgBoardWrap';
 import AddImgWrap from '../components/Manage12/AddImgWrap';
+import ImgListsWrap from '../components/Manage12/ImgListsWrap';
 import FloatingBtn from '../components/commonStyle/FloatingBtn';
 import { FlexBox, Gutter, BottomBox, ChangeFont } from '../components/commonStyle';
 import styled from 'styled-components';
@@ -19,10 +20,16 @@ function Manage12() {
       <Wrapper>
         <Head title="브랜드평가 사진제출" subtit="KGB의 브랜드평가입니다"/>
             <ContentArea>
+                {/* 5_12_1_품질평가_브랜드평가_사진제출 : ImgBoardWrap 
+                5_12_2_품질평가_브랜드평가_사진등록 : AddImgWrap
+                5_12_3_품질평가_브랜드평가_리스트 : ImgLists */}
                 {/* <ImgBoardWrap/> */}
-                <AddImgWrap/>
+                {/* <AddImgWrap/> */}
+                <ImgListsWrap/>
             </ContentArea>
-            <FloatingBtn bg="#009B90" icon="ico_add"/>
+            {/* 아래 버튼은 ImgBoardWrap일때만 보이게 */}
+            {/* <FloatingBtn bg="#009B90" icon="ico_add"/> */}
+
       </Wrapper>
       
   );
