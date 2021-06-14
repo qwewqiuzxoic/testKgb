3efd278f85088abd2ae6e0ba9dbe49095167b3a4
YugyeongO <uiux560@gmail.com>
2021-06-14 14:29:10 +0900

    Update InputGroup.js
    Merge

--- src/components/commonStyle/InputGroup.js
+++ src/components/commonStyle/InputGroup.js
@@ -39,8 +39,7 @@ const Input = styled.input`
     }
 `;
 
-function InputGroup({id, title, ph, textAlign, value, setInputValue, btn, onClick}) {
-  const [inputValue, setInputValue] = useState(value);
+function InputGroup({id, title, ph, textAlign, value, setInputValue, btn, onclick}) {
   const handleChange = e => {
     e.target.value === '' ? e.target.classList.remove('active'):e.target.classList.add('active');
     setInputValue(e.target.value);
