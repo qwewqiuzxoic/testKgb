f11a521beed2555cc945c93703186310cc72396c
KwangJa-dev <eewt123450@naver.com>
2021-08-31 13:34:00 +0900

    업데이트

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,11 +2,11 @@ import React from 'react';
 import LoginForm from '../components/login/LoginForm'
 function Login({location}) {
     const backLocation = location.state ? location.state.data.pathname: "/";
-    const token = JSON.parse(localStorage.getItem('token')) || 'not token';       
+    const token = localStorage.getItem('token') || 'not token';       
 
   return (
     <div>
-      로그인테스트
+      로그인테스트11
       {token}
       <LoginForm backLocation={backLocation}/>
     </div>
