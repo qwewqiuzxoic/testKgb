df103c0583df903c20e7dfb4c8d97fb81a050d6a
KwangJa-dev <eewt123450@naver.com>
2021-08-31 13:12:05 +0900

    업데이트

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,10 +2,12 @@ import React from 'react';
 import LoginForm from '../components/login/LoginForm'
 function Login({location}) {
     const backLocation = location.state ? location.state.data.pathname: "/";
+    const token = JSON.parse(localStorage.getItem('token')) || 'not token';       
+
   return (
     <div>
       로그인테스트
-      {Android.getFirebaseToken()}
+      {token}
       <LoginForm backLocation={backLocation}/>
     </div>
   );
