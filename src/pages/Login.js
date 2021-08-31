ecaa460eb75fb5c56a0732d5142304e6549cd693
KwangJa-dev <eewt123450@naver.com>
2021-08-31 10:42:30 +0900

    업데이트

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,6 +4,7 @@ function Login({location}) {
     const backLocation = location.state ? location.state.data.pathname: "/";
   return (
     <div>
+      {Android.getFirebaseToken()}
       <LoginForm backLocation={backLocation}/>
     </div>
   );
