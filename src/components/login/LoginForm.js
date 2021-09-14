773ebf421fe164c32929a7263982e1767c4c6f67
KwangJa-dev <eewt123450@naver.com>
2021-09-14 10:37:24 +0900

    업데이트

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -129,7 +129,7 @@ function LoginForm({ backLocation }) {
     const token = localStorage.getItem('token') || 'not token';       
     const uuid = localStorage.getItem('UUID') || 'not UUID';     
     const onSubmit = data => {
-        dispatch(login(data.id, data.password,localStorage.getItem('token'),localStorage.getItem('UUID') )); //안드로이드 토큰 관련 추가
+        dispatch(login(data.id, data.password,window.TOKEN,window.UUID)); //안드로이드 토큰 관련 추가
     }
     
     const [sucCheck,setSucCheck] = useState(false);
