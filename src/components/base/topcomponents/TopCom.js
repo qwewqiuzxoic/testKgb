8e6a655e3205044b4d32cc31e141e68057002a78
KwangJa-dev <eewt123450@naver.com>
2021-10-21 17:35:34 +0900

    업데이트

--- src/components/base/topcomponents/TopCom.js
+++ src/components/base/topcomponents/TopCom.js
@@ -55,8 +55,6 @@ function TopCom({clickMenu, bg}) {
 
     function handleClick() {
         history.goBack();
-      }
-    useEffect(() => {
         if(urlArr[1] === "board"){
             if(urlArr[2]==="1"){
                 setTitle("우리팀 톡톡");
@@ -143,10 +141,108 @@ function TopCom({clickMenu, bg}) {
             setTitle("")
         }
         
+      }
+    useEffect(() => {
+        console.log(urlArr)
+        console.log(urlArr)
+        console.log(urlArr)
+        console.log(urlArr)
+        console.log(urlArr)
+        console.log(urlArr)
+        console.log(urlArr)
+        console.log(urlArr)
+        if(urlArr[1] === "board"){
+            if(urlArr[2]==="1"){
+                setTitle("우리팀 톡톡");
+            }else if(urlArr[2] === "2"){
+                setTitle("칭찬하기");
+            }else if(urlArr[2] === "3"){
+                setTitle("꾸중하기");
+            }else if(urlArr[2] === "4"){
+                setTitle("자유게시판");
+            }else if(urlArr[2] === "5"){
+                setTitle("공지사항");
+            }else if(urlArr[2] === "6"){
+                setTitle("교육공지사항");
+            }else if(urlArr[2] === "7"){
+                setTitle("교육자료실");
+            }else if(urlArr[2] === "8"){
+                setTitle("A/S처리 노하우");
+            }else if(urlArr[2] === "9"){
+                setTitle("일반자료실");
+            }else if(urlArr[2] === "10"){
+                setTitle("작업일변경요청");
+            }
+        } else if(urlArr[1] === "Team1"){
+            if(urlArr[2]==="1"){
+                setTitle("작업일정")
+            }else if(urlArr[2] === "2"){
+                setTitle("교육일정")
+            }
+        }else if(urlArr[1] === "Team2_1"){
+            setTitle("방문견적 및 개인오더 입력")
+        }else if(urlArr[1] === "Team5"){
+            setTitle("팀 단체사진")
+        }else if(urlArr[1] === "Team7"){
+            setTitle("긴급연락망")
+        }else if(urlArr[1] === "Team9"){
+            setTitle("권역 및 대표")
+        }else if(urlArr[1] === "Team10"){
+            setTitle("담당 외주업체")
+        }else if(urlArr[1] === "Manage2_1"){
+            if(urlArr[2]==="1"){
+                setTitle("계약 해피콜")
+            }else if(urlArr[2] === "2"){
+                setTitle("미계약 해피콜")
+            }
+        }else if(urlArr[1] === "Manage3_1"){
+            setTitle("A/S 현황")
+        }else if(urlArr[1] === "Manage5_1"){
+            if(urlArr[2]==="1"){
+                setTitle("현장실사 체크")
+            }else if(urlArr[2] === "2"){
+                setTitle("대리점실사 체크")
+            }
+        }else if(urlArr[1] === "Manage11_1_4"){
+            setTitle("자가평가")
+        }else if(urlArr[1] === "Manage11"){
+            setTitle("자가평가")
+        }else if(urlArr[1] === "Team8_1"){
+            setTitle("월평가")
+        }else if(urlArr[1] === "Manage12"){
+            setTitle("브랜드평가 사진제출")
+        }else if(urlArr[1] === "Msg"){
+            setTitle("쪽지")
+        }else if(urlArr[1] === "manage13"){
+            if(urlArr[2]==="1"){
+                setTitle("교육설문")
+            }else if(urlArr[2] === "2"){
+                setTitle("이수대상교육")
+            }
+        }else if(urlArr[1] === "Manage9_1"){
+            setTitle("실시간 화상교육")
+        }else if(urlArr[1] === "Manage10_1"){
+            setTitle("교육출결체크")
+        }else if(urlArr[1] === "Manage6"){
+            setTitle("매뉴얼 학습")
+        }else if(urlArr[1] === "Bill"){
+            setTitle("청구서 관리")
+        }else if(urlArr[1] === "ProdLists"){
+            setTitle("자재주문")
+        }else if(urlArr[1] === "Manage3_1"){
+            setTitle("")
+        }else if(urlArr[1] === "Manage3_1"){
+            setTitle("")
+        }else if(urlArr[1] === "Manage3_1"){
+            setTitle("")
+        }
+        if(urlArr[1]==="app"){
+            setTitle("")
+        }
         return () => {
             
         }
-    }, [])
+    }, [window.location.pathname])
   return (
     <Wrapper bg={bg}>
         <IconBack onClick={()=>handleClick()}>
